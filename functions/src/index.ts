2423a2656dc67b8cbe5bdc32333580766c08f24b
Amit Inbar <inamit323@gmail.com>
2021-10-14 16:01:57 +0300

    feat(index.ts): Fix get all users #2

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,17 +3,8 @@ import { auth, firestore, initializeApp } from "firebase-admin";
 
 initializeApp();
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-
 export const createUser = functions.https.onCall(async (data, context) => {
   if (!context.auth) {
-    console.log(context.auth);
     throw new functions.https.HttpsError(
       "unauthenticated",
       "User cannot access this information"
@@ -38,14 +29,27 @@ export const createUser = functions.https.onCall(async (data, context) => {
 });
 
 export const getAllUsers = functions.https.onCall(async (data, context) => {
-  if (!context.auth || context.auth.token.role !== "admin") {
+  if (!context.auth) {
     throw new functions.https.HttpsError(
       "unauthenticated",
       "User cannot access this information"
     );
   }
 
-  const list = await auth().listUsers();
+  const { users } = await auth().listUsers();
 
-  return list.users;
+  return await Promise.all(
+    users.map(async ({ uid, email, metadata, customClaims, phoneNumber }) => {
+      const userInfo = await firestore().collection("users").doc(uid).get();
+
+      return {
+        ...userInfo.data(),
+        uid,
+        email,
+        metadata,
+        customClaims,
+        phoneNumber,
+      };
+    })
+  );
 });
